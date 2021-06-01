dd33e537619dc5487cc98faca7272afdc9fbbac6
ksunisth <ksunisth@gmail.com>
2021-06-01 15:32:47 +0530

    define API for model and train

---
+++ model.py
@@ -0,0 +1,5 @@
+"""
+defines a model object
+"""
+
+# weights
--- train.py
+++ train.py
@@ -1,4 +1,7 @@
 import numpy as np
+from data import load_data
+
+SEED = 0
 
 def initialized_feedforward_parameters(model):
     phi = 0
@@ -9,20 +12,29 @@ def initialized_equilibrium_parameters(model):
     return theta
 
 def train(hyperparams: dict):
+    """
+    train model with hyperparams
+    """
+    train, val, test = load_data(0.8)
     
     while(True):
-        for t in hyperparams("t-"):
+        for t in range(hyperparams["t-"]):
             pass # neg phase
         
-        for t in hyperparams("t+"):
-            pass #pos phase
+        for t in range(hyperparams["t+"]):
+            pass # pos phase
+        
+        # theta =
         
         pass
 
 if __name__=="__main__":
     hyperparams = {
-        "epsilon": 0.01,
-        "eta": 0.1,
-        "t-": 1,
-        "t+":1
+        "epsilon": 0.01,    # step size
+        "eta": 0.1,         # learning rate
+        "t-": 1,            # # of neg phase steps
+        "t+": 1,            # # of pos phase steps
+        "alpha": []         # architechture - specified as sizes of hidden layers
     }
+
+    train(hyperparams)
